17823bfc5594fb89023ce97c1ae77d878faedd7d
shaistaabd-ulhakeem <161045373+shaistaabd-ulhakeem@users.noreply.github.com>
2024-03-02 06:26:46 -0800

    variables
    Data type var,let

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+var myfavColor = ("Black");
+console.log(myfavColor);
+var userAge = (25);
+console.log(userAge);
+var userName = ("Shaista");
+console.log(userName);
---
+++ main.ts
@@ -0,0 +1,7 @@
+var myfavColor:string=("Black")
+console.log(myfavColor)
+
+var userAge:number=(25)
+console.log(userAge)
+var userName=("Shaista")
+console.log(userName)
